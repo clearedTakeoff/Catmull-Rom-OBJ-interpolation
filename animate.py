d4c10a9ce2dfcf2c45add9907fcaf2b9b88cbf61
nzupancic <nik.zupancic1@gmail.com>
2020-05-16 10:22:11 +0200

    Reading input files

--- animate.py
+++ animate.py
@@ -1,4 +1,4 @@
-
+import math
 
 # File formated as vertices followed by vt, followed by faces (repeating 3 times)
 # vt and faces don't change so only extract them once to write in output file
@@ -28,8 +28,7 @@ def read_obj(file, extract_statics=False):
     return vertices
 
 
-def catmull_rom(frame1, frame2, frame3, frame4, steps=10):
-    groups = []
+def catmull_rom(frame1, frame2, frame3, frame4, counter, steps=10):
     parts = []
     for i in range(len(frame1)):  # Number of groups
         parts.append([])
@@ -41,7 +40,6 @@ def catmull_rom(frame1, frame2, frame3, frame4, steps=10):
             control_2 = frame4[group][i]
             point_1 = frame2[group][i]
             point_2 = frame3[group][i]
-            print(point_2)
             # Tangents
             t1x = (point_2[0] - control_1[0]) * 0.5
             t1y = (point_2[1] - control_1[1]) * 0.5
@@ -65,24 +63,56 @@ def catmull_rom(frame1, frame2, frame3, frame4, steps=10):
                 parts[group][time].append((new_x, new_y, new_z))
 
     for t in range(steps):
-        f = open("int_frame" + "%03d" % (t + 1) + ".obj", "w")
+        f = open("output/int_frame" + "%03d" % counter + ".obj", "w")
+        counter += 1
         for group in range(len(parts)):
             for line in parts[group][t]:
                 f.write("v " + str(line[0]) + " " + str(line[1]) + " " + str(line[2]) + "\n")
             for line in static_parts[group]:
                 f.write(line)
         f.close()
+    return counter
 
 
+def read_input(file):
+    f = open(file, "r")
+    frames = []
+    duration = []
+    for line in f:
+        parameters = line.split(" ")
+        frames.append(parameters[0])  # Obj file name
+        duration.append(int(parameters[1]))
+    for i in range(len(duration) - 1):
+        duration[i] = duration[i + 1] - duration[i]
+
+    frames.insert(0, frames[-1])
+    frames.append(frames[1])
+    return frames, duration
 
 
-
+def interpolate_sequence(frames, duration):
+    global static_parts
+    file_ind = 1
+    FPS = 30
+    for i in range(1, len(frames) - 2):
+        frame1 = read_obj("input/" + frames[i - 1], True)  # Control "point"
+        frame2 = read_obj("input/" + frames[i])  # From "point"
+        frame3 = read_obj("input/" + frames[i + 1])  # To "point"
+        frame4 = read_obj("input/" + frames[i + 2])  # Control "point" 2
+        frames_no = math.ceil((FPS * duration[i - 1]) / 1000)
+        file_ind = catmull_rom(frame1, frame2, frame3, frame4, file_ind, steps=frames_no)
+        static_parts = []
 
 
 if __name__ == "__main__":
-    frame1 = read_obj("input/frame_01.obj", True)
-    frame2 = read_obj("input/frame_02.obj")
-    frame3 = read_obj("input/frame_03.obj")
-    frame4 = read_obj("input/frame_04.obj")
-    print(frame1)
-    catmull_rom(frame1, frame2, frame3, frame4, 60)
+    f, d = read_input("input/input_00.txt")
+    # f, d = read_input("input/input_03.txt")
+    # f, d = read_input("input/input_03.txt")
+    # f, d = read_input("input/input_03.txt")
+    interpolate_sequence(f, d)
+    #frame1 = read_obj("input/frame_01.obj", True)
+    #frame2 = read_obj("input/frame_02.obj")
+    #frame3 = read_obj("input/frame_03.obj")
+    #frame4 = read_obj("input/frame_04.obj")
+    #print(frame1)
+    #catmull_rom(frame1, frame2, frame3, frame4, 60)
